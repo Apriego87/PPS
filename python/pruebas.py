ab89c178ad46f6bccb60dfa528aaa169af6fa33e
Apriego87 <antoniopriego22@gmail.com>
2022-11-08 21:11:15 +0100

    cosas

---
+++ python/pruebas.py
@@ -0,0 +1,10 @@
+""" name = input("Introduce tu nombre: ")
+print("Hola, " + name) """
+
+print(f"Hola, {input('Introduce tu nombre: ')}")
+
+nombres = ["Nombre1", "Nombre2", "Nombre3", "Nombre4"]
+
+print(nombres)
+
+nombres.append("Nombre5")
